38a895406a070e114f9d406ad687bd3b134e38fe
prasiddha98 <famousdhungana98@gmail.com>
2024-12-28 13:05:28 +0545

    first commit of day2 with adding some fun work

---
+++ day2.php
@@ -0,0 +1,15 @@
+<?php
+echo "goodmorning, this is day2 of learning php"; 
+
+//isset function
+$name="ram";
+echo isset($name); //true
+echo isset($age); //false
+
+//empty function
+$name="ram";
+echo empty($name); //false
+echo empty($age); //true
+
+
+?>
